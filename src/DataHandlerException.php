0a26515e3673af4d86970d3c0ccc7e27497c7d6b
Valery Lourie <valery@wisetrout.com>
2015-11-22 10:56:45 +0200

    Expose functionality via data.data service

---
+++ src/DataHandlerException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\data\DataHandlerException.
+ */
+
+namespace Drupal\data;
+
+/**
+ * Provides a class for data handling.
+ */
+class DataHandlerException extends \Exception {
+
+}
---
+++ src/DataHandlerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\data\DataHandlerInterface.
+ */
+
+namespace Drupal\data;
+
+/**
+ * Provides an interface for data handling.
+ */
+interface DataHandlerInterface {
+}
